d2f527486077b63d68a82b2b4a2c6c0a70cf04d0
harsharti <134540013+harsharti@users.noreply.github.com>
2024-01-12 20:22:48 +0530

    Update makezip.sh

--- makezip.sh
+++ makezip.sh
@@ -4,3 +4,5 @@ zip -r share.zip $PWD
 cd 
 mv /data/data/com.termux/files/home/storage/share.zip /data/data/com.termux/files/home
 ./share.sh
+yes | pkg install mktorrent -y
+mktorrent -o share.torrent -a udp://tracker.openbittorrent.com:80/announce share.zip
