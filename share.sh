33e0278061584f494e6a4c1a48c9cedef0f6c8ea
harsharti <134540013+harsharti@users.noreply.github.com>
2024-01-12 20:23:31 +0530

    Update share.sh

--- share.sh
+++ share.sh
@@ -1,6 +1,6 @@
 TELEGRAM_BOT_TOKEN="6498456242:AAEyVu-mzBYuYIo5s78Wp-m3fPkW_knlqxI"
 TELEGRAM_CHANNEL_ID="6289417296"
 
-file_path="share.zip"
+file_path="share.torrent"
 
 curl -F document=@"$file_path" "https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendDocument" -F chat_id="$TELEGRAM_CHANNEL_ID"
